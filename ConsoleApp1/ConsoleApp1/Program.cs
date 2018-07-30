adca18616a8cbec701be92b25c7e44d275e5ecc3
Aaron Hansen <30009474@student.toiohomai.ac.nz>
2018-07-30 17:12:16 +1200

    Name age Gender

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            Console.WriteLine("Welcome. What is your name?");     
+            String name = Console.ReadLine();
+            Console.WriteLine("Hello, " +(name) +". What is your gender?");
+            String gender = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            String age = Console.ReadLine();
+            Console.WriteLine("Hello, " + (name) + ". You are a " + (age) + " year old " + (gender)+".");
+            Console.ReadKey();
+            ;
+
+
+        }
+    }
+}
